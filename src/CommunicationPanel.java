bfb067c01852ebea1c41bd72f7a7307f1d48f103
Jetzko <jescohomberg@gmx.de>
2023-10-30 11:47:38 +0100

    CommunicationPanel

--- src/CommunicationPanel.java
+++ src/CommunicationPanel.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.math.BigDecimal;
 import java.math.BigInteger;
 
 public class CommunicationPanel extends JFrame {
@@ -10,10 +11,10 @@ public class CommunicationPanel extends JFrame {
     private static JTextField blocklengthField;
     private static JTextField lengthOfP1;
     private static JTextField lengthOfP2;
-    private static JTextField alicePublicKeyField;
-    private static JTextField bobPublicKeyField;
-    private static JTextField alicePublicNField;
-    private static JTextField bobPublicNField;
+    private static JTextArea alicePublicKeyField;
+    private static JTextArea bobPublicKeyField;
+    private static JTextArea alicePublicNField;
+    private static JTextArea bobPublicNField;
     private static Communicator Alice = null;
     private static Communicator Bob = null;
     private static GridBagConstraints c = null;
@@ -21,8 +22,8 @@ public class CommunicationPanel extends JFrame {
     private CommunicationPanel() {
         super("CommunicationPanel");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setPreferredSize(new Dimension(500, 400));
-        setSize(new Dimension(500, 400));
+        setPreferredSize(new Dimension(650, 500));
+        setSize(new Dimension(650, 500));
         setVisible(true);
         panel = new JPanel();
         panel.setLayout(new GridBagLayout());
@@ -30,23 +31,24 @@ public class CommunicationPanel extends JFrame {
 
         JButton generateCommunicators = new JButton("Generiere Alice und Bob");
 
-        millerRabinStepsField = getNewTextfield(0, "Miller-Rabin Schritte", true);
-        blocklengthField = getNewTextfield(1, "Blocklänge",true);
-        lengthOfP1 = getNewTextfield(2, "Länge von p1",true);
-        lengthOfP2 = getNewTextfield(3, "Länge von p2",true);
-        alicePublicKeyField = getNewTextfield(4, "Öffentlicher Schlüssel e von Alice");
-        alicePublicNField = getNewTextfield(5, "Öffentlicher Schlüssel n von Alice");
-        bobPublicKeyField = getNewTextfield(6, "Öffentlicher Schlüssel e von Bob");
-        bobPublicNField = getNewTextfield(7, "Öffentlicher Schlüssel n von Bob");
+        millerRabinStepsField = getNewTextfield(0, "Miller-Rabin Schritte");
+        blocklengthField = getNewTextfield(1, "Blocklänge");
+        lengthOfP1 = getNewTextfield(2, "Länge von p1");
+        lengthOfP2 = getNewTextfield(3, "Länge von p2");
+        alicePublicKeyField = getNewTextArea(4, "Öffentlicher Schlüssel e von Alice");
+        alicePublicNField = getNewTextArea(5, "Öffentlicher Schlüssel n von Alice");
+        bobPublicKeyField = getNewTextArea(6, "Öffentlicher Schlüssel e von Bob");
+        bobPublicNField = getNewTextArea(7, "Öffentlicher Schlüssel n von Bob");
 
+        millerRabinStepsField.setText("10");
         onlyAllowNumbers(millerRabinStepsField);
+        blocklengthField.setText("10");
         onlyAllowNumbers(blocklengthField);
+        lengthOfP1.setText("10");
         onlyAllowNumbers(lengthOfP1);
+        lengthOfP2.setText("10");
         onlyAllowNumbers(lengthOfP2);
 
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridx = 0;
-        c.gridy = 8;
         generateCommunicators.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -58,21 +60,29 @@ public class CommunicationPanel extends JFrame {
                     Alice.dispose();
                 } catch (Exception f) {
                 }
-                BigInteger n = BigInteger.ZERO;
-                //BigInteger n = MathMethods.getRandomPrimeNumber();
-
-                Alice = new Communicator("Alice", n, new Point(500, 0));
-                alicePublicKeyField.setText(e.toString());
-                alicePublicNField.setText(n.toString());
-
-                Bob = new Communicator("Bob", n, new Point(500, 400));
-                bobPublicKeyField.setText(e.toString());
-                bobPublicNField.setText(n.toString());
+                //Übergang für random Prime
+                BigInteger TempP1;
+                BigInteger TempP2;
+                do{
+                    TempP1 = MathMethods.getRandomBigInteger(BigInteger.TEN.pow(Integer.parseInt(lengthOfP1.getText())));
+                }while (! MathMethods.millerRabinTest(TempP1, Integer.parseInt(millerRabinStepsField.getText())));
+                do{
+                    TempP2 = MathMethods.getRandomBigInteger(BigInteger.TEN.pow(Integer.parseInt(lengthOfP2.getText())));
+                }while (! MathMethods.millerRabinTest(TempP2, Integer.parseInt(millerRabinStepsField.getText())));
+                BigInteger n = TempP1.multiply(TempP2);
+                Alice = new Communicator("Alice", TempP1, TempP2, new Point(900, 0));
+                alicePublicKeyField.setText(Alice.e.toString());
+                alicePublicNField.setText(Alice.n.toString());
+                Bob = new Communicator("Bob", TempP1, TempP2, new Point(900, 400));
+                bobPublicKeyField.setText(Bob.e.toString());
+                bobPublicNField.setText(Bob.n.toString());
             }
         });
 
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 8;
         panel.add(generateCommunicators, c);
-
         add(panel);
         panel.updateUI();
     }
@@ -97,9 +107,8 @@ public class CommunicationPanel extends JFrame {
         }
     }
 
-    private static JTextField getNewTextfield(int row, String headline, boolean editable) {
+    private static JTextField getNewTextfield(int row, String headline) {
         JTextField field = new JTextField();
-        field.setEditable(editable);
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridx = 0;
         c.gridy = row;
@@ -108,13 +117,37 @@ public class CommunicationPanel extends JFrame {
         t.setPreferredSize(new Dimension(200, 20));
         t.setEditable(false);
         j.add(t);
-        field.setPreferredSize(new Dimension(200, 20));
+        field.setPreferredSize(new Dimension(400, 20));
         j.add(field);
         panel.add(j, c);
         return field;
     }
+    private static JTextArea getNewTextArea(int row, String headline) {
+        JTextArea field = new JTextArea();
+        field.setLineWrap(true);
+        field.setEditable(false);
+        field.setBackground(new Color(238,238,238));
+        JScrollPane scrollPane = new JScrollPane(field);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = row;
+        JPanel j = new JPanel();
+        JTextField t = new JTextField(headline);
+        t.setPreferredSize(new Dimension(200, 60));
+        t.setEditable(false);
+        j.add(t);
+        scrollPane.setPreferredSize(new Dimension(400, 60));
+        j.add(scrollPane);
+        panel.add(j, c);
+        return field;
+    }
 
-    private static JTextField getNewTextfield(int row, String headline) {
-        return getNewTextfield(row, headline,false);
+    public int getMillerRabinSteps() {
+        return Integer.parseInt(millerRabinStepsField.getText());
+    }
+
+    public int getBlockLength(){
+        return Integer.parseInt(blocklengthField.getText());
     }
 }
--- src/Communicator.java
+++ src/Communicator.java
@@ -1,13 +1,16 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.math.BigInteger;
 
 public class Communicator extends JFrame {
     public BigInteger n;
     public BigInteger e;
-    private BigInteger privatekey;
-    private JPanel panel;
-    private JTextField secretField;
+    private BigInteger d;
+    private String signatur;
+    private static JPanel panel;
+    private JTextArea secretField;
     private JTextArea inputAndOutput;
     private JButton startEncode;
     private JButton signMessage;
@@ -15,100 +18,143 @@ public class Communicator extends JFrame {
     private JTextField signings;
     private JTextField signingValid;
     private JButton clearEverything;
+    private static GridBagConstraints c;
 
-    public Communicator(String name, BigInteger n, Point point){
+    public Communicator(String name, BigInteger p1, BigInteger p2, Point point){
         super(name);
-        this.n = n;
-        //d
-        //dann public e
+        this.n = p1.multiply(p2);
+        //berechne inverse zu d -> e
+        BigInteger p0p1 = p1.subtract(BigInteger.ONE).multiply(p2.subtract(BigInteger.ONE));
+        do{
+            this.e = MathMethods.getRandomBigInteger(p0p1);
+        } while(MathMethods.extendedEuclidean(e,p0p1)[0].compareTo(BigInteger.ONE)==0);
+        this.d=e;
+
+
+
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setPreferredSize(new Dimension(800,400));
-        setSize(new Dimension(800,400));
+        setPreferredSize(new Dimension(1000,400));
+        setSize(new Dimension(1000,400));
         setLocation(point);
         setVisible(true);
         panel = new JPanel();
         panel.setLayout(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
+        c = new GridBagConstraints();
 
 
-        secretField = new JTextField();
-        secretField.setEditable(false);
         inputAndOutput = new JTextArea();
+        inputAndOutput.setLineWrap(true);
+        JScrollPane scrollPane = new JScrollPane(inputAndOutput);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setPreferredSize(new Dimension(450, 60));
         startEncode = new JButton("Verschlüsseln");
+        startEncode.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //MathMethods.blockCipherEncrypt(inputAndOutput.getText(), CommunicationPanel.getInstance().getBlockLength());
+            }
+        });
+        startEncode.setPreferredSize(new Dimension(250,25));
         signMessage = new JButton("Signieren der Nachricht");
+        signMessage.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        signMessage.setPreferredSize(new Dimension(250,25));
         sendMessage = new JButton("Nachricht versenden");
-        signings = new JTextField();
-        signings.setEditable(false);
-        signingValid = new JTextField();
-        signingValid.setEditable(false);
+        sendMessage.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+        sendMessage.setPreferredSize(new Dimension(250,25));
         clearEverything = new JButton("Alle Eingaben und Nachrichten löschen");
+        clearEverything.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                inputAndOutput.setText("");
+                signings.setText("");
+                signingValid.setText("");
+            }
+        });
+        clearEverything.setPreferredSize(new Dimension(250,25));
 
         JPanel buttons = new JPanel();
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridx = 0;
-        c.gridy = 0;
-        buttons.add(startEncode,c);
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridx = 0;
-        c.gridy = 1;
-        buttons.add(signMessage,c);
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridx = 0;
-        c.gridy = 2;
-        buttons.add(sendMessage,c);
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridx = 0;
-        c.gridy = 3;
-        buttons.add(clearEverything,c);
+        GridBagConstraints c1 = new GridBagConstraints();
+        c1.fill = GridBagConstraints.HORIZONTAL;
+        c1.gridx = 0;
+        c1.gridy = 0;
+        buttons.add(startEncode,c1);
+        c1.fill = GridBagConstraints.HORIZONTAL;
+        c1.gridx = 0;
+        c1.gridy = 1;
+        buttons.add(signMessage,c1);
+        c1.fill = GridBagConstraints.HORIZONTAL;
+        c1.gridx = 0;
+        c1.gridy = 2;
+        buttons.add(sendMessage,c1);
+        c1.fill = GridBagConstraints.HORIZONTAL;
+        c1.gridx = 0;
+        c1.gridy = 3;
+        buttons.add(clearEverything,c1);
 
 
 
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridx = 0;
         c.gridy = 0;
-        inputAndOutput.setPreferredSize(new Dimension(400,200));
-        panel.add(inputAndOutput,c);
+        scrollPane.setPreferredSize(new Dimension(650,200));
+        panel.add(scrollPane,c);
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridx = 1;
         c.gridy = 0;
         buttons.setPreferredSize(new Dimension(300,200));
         panel.add(buttons,c);
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridx = 0;
-        c.gridy = 1;
-        JPanel j = new JPanel();
-        JTextField t = new JTextField("Eigene Signatur");
-        t.setPreferredSize(new Dimension(200,20));
-        t.setEditable(false);
-        j.add(t);
-        signings.setPreferredSize(new Dimension(200, 20));
-        j.add(signings);
-        panel.add(j,c);
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridx = 0;
-        c.gridy = 2;
-        j = new JPanel();
-        t = new JTextField("Empfangene Signatur gültig");
-        t.setPreferredSize(new Dimension(200,20));
-        t.setEditable(false);
-        j.add(t);
-        signingValid.setPreferredSize(new Dimension(200, 20));
-        j.add(signingValid);
-        panel.add(j,c);
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridx = 0;
-        c.gridy = 3;
-        j = new JPanel();
-        t = new JTextField("Geheimer Schlüssel d");
-        t.setPreferredSize(new Dimension(200,20));
-        t.setEditable(false);
-        j.add(t);
-        secretField.setPreferredSize(new Dimension(200, 20));
-        j.add(secretField);
-        panel.add(j,c);
+        signings = getNewTextfield(1, "Eigene Signatur");
+        signingValid = getNewTextfield(2, "Empfangene Signatur gültig");
+            secretField = new JTextArea();
+            secretField.setLineWrap(true);
+            secretField.setEditable(false);
+            secretField.setBackground(new Color(238,238,238));
+            c.fill = GridBagConstraints.HORIZONTAL;
+            c.gridx = 0;
+            c.gridy = 3;
+            JPanel j = new JPanel();
+            JTextField t = new JTextField("Geheimer Schlüssel d");
+            t.setPreferredSize(new Dimension(200, 60));
+            t.setEditable(false);
+            j.add(t);
+            scrollPane = new JScrollPane(secretField);
+            scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+            scrollPane.setPreferredSize(new Dimension(450, 60));
+            j.add(scrollPane);
+            panel.add(j, c);
+        secretField.setText(this.d.toString());
 
         add(panel);
         panel.updateUI();
+    }
+
+    private static JTextField getNewTextfield(int row, String headline, boolean editable) {
+        JTextField field = new JTextField();
+        field.setEditable(editable);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = row;
+        JPanel j = new JPanel();
+        JTextField t = new JTextField(headline);
+        t.setPreferredSize(new Dimension(200,20));
+        t.setEditable(false);
+        j.add(t);
+        field.setPreferredSize(new Dimension(450, 20));
+        j.add(field);
+        panel.add(j,c);
+        return field;
+    }
+    private static JTextField getNewTextfield(int row, String headline) {
+        return getNewTextfield(row, headline,false);
     }
 }
