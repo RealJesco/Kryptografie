264d69fe30e1fa8de965805175619035c33a59da
Adham Aijou <adham380@gmail.com>
2024-01-04 17:57:13 +0100

    Made extra task GUI responsive

--- src/Zusatzaufgabe.java
+++ src/Zusatzaufgabe.java
@@ -23,24 +23,34 @@ public class Zusatzaufgabe extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //Percentage of the screen
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        setPreferredSize(new Dimension(1000, 350));
-        setSize(new Dimension(1000, 350));
+        //Responsive width and height
+        int[] frameWidth = {(int) Math.round(screenSize.width * 0.5)};
+        int[] frameHeight = {(int) Math.round(screenSize.height * 0.5)};
+        setPreferredSize(new Dimension(frameWidth[0], frameHeight[0]));
+        setSize(new Dimension(frameWidth[0], frameHeight[0]));
         setVisible(true);
         panel = new JPanel();
         panel.setLayout(new GridBagLayout());
         c = new GridBagConstraints();
 
         calculateDivisorsOfPrimeNumber = new JButton("Berechne die Primzahl als Summe zweier Quadrate");
-        calculateDivisorsOfPrimeNumber.setPreferredSize(new Dimension(690,100));
+        calculateDivisorsOfPrimeNumber.setPreferredSize(new Dimension((int) (frameWidth[0] * 0.7), (int) (frameHeight[0] * 0.2)));
         calculateDivisorsOfPrimeNumber.setFont(new Font("Arial", Font.PLAIN, 25));
         int i = 0;
         primeNumberField = new JTextField();
-        primeNumberField.setPreferredSize(new Dimension(680,100));
-        primeNumberField.setFont(new Font("Arial", Font.PLAIN, 30));
+        primeNumberField.setPreferredSize(new Dimension((int) (frameWidth[0] * 0.7), (int) (frameHeight[0] * 0.2)));
+        primeNumberField.setFont(new Font("Arial", Font.PLAIN, frameHeight[0] / 15));
+//        Make it scrollable
+
         divisorsOfPrimeNumberField = new JTextArea();
         divisorsOfPrimeNumberField.setLineWrap(true);
-        divisorsOfPrimeNumberField.setFont(new Font("Arial", Font.PLAIN, 30));
-        divisorsOfPrimeNumberField.setPreferredSize(new Dimension(680, 100));
+        divisorsOfPrimeNumberField.setFont(new Font("Arial", Font.PLAIN, frameHeight[0] / 15));
+
+        JScrollPane divisorsScrollPane = new JScrollPane(divisorsOfPrimeNumberField);
+        divisorsScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        divisorsScrollPane.setPreferredSize(new Dimension((int) (frameWidth[0] * 0.7), (int) (frameHeight[0] * 0.2)));
+
+
         //Placeholder text
         c.gridx = 0;
         c.gridy = i;
@@ -53,12 +63,15 @@ public class Zusatzaufgabe extends JFrame {
         panel.add(new JLabel("Summe zweier Quadrate"), c);
         c.gridx = 1;
         c.gridy = i++;
-        panel.add(divisorsOfPrimeNumberField, c);
+        panel.add(divisorsScrollPane, c);
+//        panel.add(divisorsOfPrimeNumberField, c);
         c.gridx = 1;
         c.gridy = i++;
         panel.add(calculateDivisorsOfPrimeNumber,c);
         calculateDivisorsOfPrimeNumber.addActionListener(e -> {
             try {
+                //Remove all whitespaces
+                primeNumberField.setText(primeNumberField.getText().replaceAll("\\s+",""));
                 //Check if input is a number only
                 if(!primeNumberField.getText().matches("[0-9]+")) {
                     throw new Exception("Bitte geben Sie eine Zahl ein");
@@ -70,6 +83,24 @@ public class Zusatzaufgabe extends JFrame {
                 divisorsOfPrimeNumberField.setText(ex.getMessage());
             }
         });
+
+        addComponentListener(new java.awt.event.ComponentAdapter() {
+            public void componentResized(java.awt.event.ComponentEvent evt) {
+                frameWidth[0] = getSize().width;
+                frameHeight[0] = getSize().height;
+
+                calculateDivisorsOfPrimeNumber.setPreferredSize(new Dimension((int) (frameWidth[0] * 0.7), (int) (frameHeight[0] * 0.2)));
+                primeNumberField.setPreferredSize(new Dimension((int) (frameWidth[0] * 0.7), (int) (frameHeight[0] * 0.2)));
+                divisorsScrollPane.setPreferredSize(new Dimension((int) (frameWidth[0] * 0.7), (int) (frameHeight[0] * 0.2)));
+                primeNumberField.setFont(new Font("Arial", Font.PLAIN, frameHeight[0] / 15));
+                divisorsOfPrimeNumberField.setFont(new Font("Arial", Font.PLAIN, frameHeight[0] / 15));
+                calculateDivisorsOfPrimeNumber.setFont(new Font("Arial", Font.PLAIN, 25));
+
+                panel.revalidate();
+                panel.repaint();
+            }
+        });
+
 
         add(panel);
         pack();
