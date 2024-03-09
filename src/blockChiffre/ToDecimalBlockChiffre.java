95ef6f984939e6b9cad62ad511ede4b5d81997f9
Adham Aijou <adham380@gmail.com>
2024-03-09 14:01:06 +0100

    Adjusted idea/gitignore and added block cipher structure

---
+++ src/blockChiffre/ToDecimalBlockChiffre.java
@@ -0,0 +1,20 @@
+package blockChiffre;
+
+import java.math.BigInteger;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public final class ToDecimalBlockChiffre {
+
+    public static List<BigInteger> encrypt(final String plaintext, final int numberBase, final int blockSize) {
+        //Die beiden Methoden sind identisch
+        return FromDecimalBlockChiffre.decrypt(plaintext, numberBase, blockSize);
+    }
+
+    public static String decrypt(final List<BigInteger> ciphertext, final int numberBase) {
+        return ciphertext
+                .stream()
+                .map((BigInteger block) -> FromDecimalBlockChiffre.unicode_g_adic_development(block, BigInteger.valueOf(numberBase)))
+                .collect(Collectors.joining(""));
+    }
+}
---
+++ src/rsa/KeyPairRsa.java
@@ -0,0 +1,5 @@
+package rsa;
+
+
+public record KeyPairRsa(PublicKeyRsa publicKeyRsa, PrivateKeyRsa privateKeyRsa){
+}
