3914339b36d43997f843241732e9ec2d1f079902
Adham Aijou <adham380@gmail.com>
2024-03-11 18:53:15 +0100

    Added tests for RsaStringService signing and verification

--- src/rsa/RsaStringServiceTest.java
+++ src/rsa/RsaStringServiceTest.java
@@ -3,6 +3,8 @@ package rsa;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,10 +21,21 @@ class RsaStringServiceTest {
     }
 
     @Test
-    void sign() {
+    void sign() throws NoSuchAlgorithmException {
+        KeyPairRsa keyPair = RsaService.generateKeyPair(512, 100, BigInteger.valueOf(13));
+        String message = "Høllå die Walȡféè ㆕";
+        String signedMessage = RsaStringService.sign(keyPair.privateKeyRsa(), message, 55296);
+        assertNotEquals(message, signedMessage);
     }
 
     @Test
-    void verify() {
+    void verify() throws NoSuchAlgorithmException {
+        KeyPairRsa keyPair = RsaService.generateKeyPair(512, 100, BigInteger.valueOf(13));
+        String message = "Høllå die Walȡféè ㆕";
+        String signedMessage = RsaStringService.sign(keyPair.privateKeyRsa(), message, 55296);
+        assertTrue(RsaStringService.verify(keyPair.publicKeyRsa(), message, signedMessage, 55296));
+        assertFalse(RsaStringService.verify(keyPair.publicKeyRsa(), message.concat(" Ewa"),signedMessage, 55296));
+        assertFalse(RsaStringService.verify(keyPair.publicKeyRsa(), message,signedMessage, 55295));
+        assertFalse(RsaStringService.verify(keyPair.publicKeyRsa(), message.substring(0, message.length() - 2),signedMessage, 55295));
     }
 }
