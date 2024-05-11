fadc589f96bcceaa356cb49835b33f8682f45dd5
admin <mattis.petersen@edu.fhdw.de>
2024-05-11 23:11:02 +0200

    tests for ElGamalVanstone - to be finished

--- src/test/elGamalMenezesVanstone/ElGamalMenezesVanstoneStringServiceTest.java
+++ src/test/elGamalMenezesVanstone/ElGamalMenezesVanstoneStringServiceTest.java
@@ -1,5 +1,5 @@
 package test.elGamalMenezesVanstone;
 
 public class ElGamalMenezesVanstoneStringServiceTest {
-
+    //TODO: How do I need to use the Builder? => Is there a ElGamalVanstoneBuilder with a keyPair?
 }
